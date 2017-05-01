162bc786aff3459d92a1fe1af54f543a337311a4
Stephan Nöhrer <stephan.noehrer@technikum-wien.at>
2017-05-01 21:56:22 +0200

    add project Task1

---
+++ tasks/Task1/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Run();      
+        }
+
+        private void Run()
+        {
+            Console.WriteLine($"{ConfiguredOutputText}");
+
+            return;
+        }
+
+        private string ConfiguredOutputText
+        {
+            get
+            {
+                var outputText = "default";
+
+                if (!String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["output"]))
+                {
+                    outputText = ConfigurationManager.AppSettings["output"].ToString();
+                }
+
+                return outputText;
+            }
+        }
+
+    }
+}
